ffc0dbc67941dcdc598e6c66ce1e2fb60a881903
EdsonSobrinho <edsonpaulosobrinho@gmail.com>
2018-07-11 18:00:50 -0300

    git add*.*

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb://edson:edson13 @ds233571.mlab.com:33571/node-str-aws',
+    sendgridKey: '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',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=veraregina;AccountKey=nYKR9AkdnmRl6ofSemH6/xJj41S0G7nka/MP92yupJzo/XThtZocwOs1axJucCPTg4YhCy97BbuY5nGJQz3ODw==;EndpointSuffix=core.windows.net'
+}
