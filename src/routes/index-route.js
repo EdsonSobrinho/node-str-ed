8c770a7ca588ae36b8533331e8a1856eb5f8e5a2
Edson Sobrinho <38223447+EdsonSobrinho@users.noreply.github.com>
2018-07-13 14:33:23 +0000

    Update index-route.js

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -4,10 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API - Loja do Edson",
-        version: "1.0.0"
-    });
-});
+    res.status(200).send('<html><head><meta charset="UTF-8"/><title>Document</title></head><body> <form action="salvar_dados.php" method="post"><input type="text" placeholder="Loja do Edson"><br/></form></body></html>'
+  });
 
 module.exports = router;
